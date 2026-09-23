c13091648468321e9409f8885f83833e852ab95c
Santiago David Lopez <santiagodavidlopez85@gmail.com>
2026-09-23 09:13:55 -0500

    Fix: actualizacion de credenciales MySQL y conexion de base de datos

--- src/main/webapp/app/config/dayjs.ts
+++ src/main/webapp/app/config/dayjs.ts
@@ -4,6 +4,7 @@ import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
 import 'dayjs/locale/es';
+import 'dayjs/locale/en';
 // jhipster-needle-i18n-language-dayjs-imports - JHipster will import languages from dayjs here
 
 dayjs.extend(customParseFormat);
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -132,6 +132,7 @@ module.exports = async options => {
           output: {
             groupBy: [
               { pattern: './src/main/webapp/i18n/es/*.json', fileName: './i18n/es.json' },
+              { pattern: './src/main/webapp/i18n/en/*.json', fileName: './i18n/en.json' },
               // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
             ],
           },
